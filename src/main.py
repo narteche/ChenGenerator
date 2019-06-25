b9e88592111f98ad103cc56709f12421c6cce2f4
Noel Arteche <noel.arteche@gmail.com>
2019-06-25 17:15:35 +0200

    More changes to tests.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ Created on Tue Jun 25 11:22:56 2019
 """
 from time import time
 
+
 from tools.QBF import QBF
 from tools.system_tools import run_command, clear
 from generators.generator_for_T1 import generate_ChenType1
@@ -18,7 +19,8 @@ def main_for_user():
         print("Select an option:")
         print("     1. Generator for Chen Formulas of Type 1.")
         print("     2. Generator for Chen Formulas of Type 2 [NOT YET IMPLEMENTED].")
-        print("     3. EXIT.")
+        print("     3. Run Type 1 tests repeatedly.")
+        print("     4. EXIT.")
         s = input("[SELECT] --> ")
         clear()
         if s == '1':
@@ -26,6 +28,8 @@ def main_for_user():
         elif s == '2':
             run_type2_test()
         elif s == '3':
+            run_repeated_tests(303)
+        elif s == '4':
             break
         else:
             print("Incorrect selection; try again.")
@@ -53,21 +57,21 @@ def run_type1_test():
 def run_type2_test():
     print("TO BE IMPLEMENTED. TRY AGAIN")
 
-#main_for_user()
 
-generate = list()
-solve = list()
-for i in range(1, 300):
-    
-    
-    t0 = time()
-    phi = generate_ChenType1(i)
-    t = time() - t0
-    generate.append(t)
-    print("Time for generate size n = {}: {} s".format(i, t))
 
-    t = phi.check_satisfiability()
-    solve.append(t)
-    print("Time for solving size n = {}: {} s".format(i, t))
-    
-    
+def run_repeated_tests(n):
+    generate = list()
+    solve = list()
+    for i in range(1, n):
+
+        t0 = time()
+        phi = generate_ChenType1(i)
+        t = time() - t0
+        generate.append(t)
+        print("Time for generate size n = {}: {} s".format(i, t))
+
+        t = phi.check_satisfiability()
+        solve.append(t)
+        print("Time for solving size n = {}: {} s".format(i, t))
+
+main_for_user()
